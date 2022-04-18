fc281b35a8099c0ca1f1d70a8bc2dbb0a762c5c3
mirzasabbirgithub <mirzaasabbir@gmail.com>
2022-04-18 11:10:01 +0600

    About page completed

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Button, Card } from 'react-bootstrap';
 
 const About = () => {
           return (
                     <div>
-                              <h2>About page</h2>
+                              <Card className="text-center">
+                                        <Card.Header>About Me </Card.Header>
+                                        <Card.Body>
+                                                  <Card.Title>Hi, This is <span className='text-success'>Mirza Sabbir Ahmed</span></Card.Title>
+                                                  <Card.Text>
+                                                            My goal is to become a full stack web developer within next 2 months. As a web developer make website for the users with good and quality experience. I want to build my own company for web development.
+                                                  </Card.Text>
+                                        </Card.Body>
+                              </Card>
                     </div>
           );
 };
